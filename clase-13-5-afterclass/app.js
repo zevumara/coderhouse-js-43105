98228549ea6f76c85aecc7b7ff2954b59d028296
barduck <sebastiancamara@gmail.com>
2023-07-02 13:44:57 -0300

    Comentarios al código y estilos para más facha

--- clase-13-5-afterclass/app.js
+++ clase-13-5-afterclass/app.js
@@ -11,92 +11,127 @@ Objetivos de la tercer pre-entrega:
 // de nuestro e-commerce.
 class BaseDeDatos {
   constructor() {
+    // Array de la base de datos
     this.productos = [];
-    // Vamos a cargar todos los productos que tengamos
+    // Con una simple línea de código, vamos a ir cargando todos los productos que tengamos
     this.agregarRegistro(1, "Arroz", 100, "Alimentos", "arroz.jpg");
     this.agregarRegistro(2, "Fideos", 50, "Alimentos", "fideos.jpg");
     this.agregarRegistro(3, "Alfajor", 25, "Alimentos", "alfajor.jpg");
+    this.agregarRegistro(4, "Pan", 25, "Alimentos", "pan.jpg");
   }
 
+  // Método que crea el objeto producto y lo almacena en el array con un push
   agregarRegistro(id, nombre, precio, categoria, imagen) {
     const producto = new Producto(id, nombre, precio, categoria, imagen);
     this.productos.push(producto);
   }
 
+  // Nos retorna el array con todos los productos de la base de datos
   traerRegistros() {
     return this.productos;
   }
 
+  // Busca un producto por ID, si lo encuentra lo retorna en forma de objeto
+  // A tener en cuenta: Los IDs son únicos, debe haber uno solo por producto para evitar errores
   registroPorId(id) {
     return this.productos.find((producto) => producto.id === id);
   }
 
+  // Retorna una lista (array) de productos que incluyan en el nombre los caracteres
+  // que le pasamos por parámetro. Si le pasamos "a" como parámetro, va a buscar y
+  // devolver todos los productos que tengan la letra "a" en el nombre del producto
   registrosPorNombre(palabra) {
     return this.productos.filter((producto) => producto.nombre.toLowerCase().includes(palabra));
   }
 }
 
-// Clase carrito
+// Clase carrito, para manipular los productos de nuestro carrito
 class Carrito {
   constructor() {
+    // Cargamos del storage
     const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+    // Inicializamos variables
     this.carrito = carritoStorage || [];
     this.total = 0;
     this.totalProductos = 0;
+    // Apenas se crea el carrito, que llame al método listar para que
+    // renderice todos los productos que haya en el storage
     this.listar();
   }
 
+  // Verificamos si el producto está en el carrito. Usamos desectruración en el parámetro:
+  // recibimos el objeto producto en el parámetro pero solo usamos la propiedad id
   estaEnCarrito({ id }) {
     return this.carrito.find((producto) => producto.id === id);
   }
 
+  // Método para agregar el producto al carrito
   agregar(producto) {
-    let productoEnCarrito = this.estaEnCarrito(producto);
+    // Si el producto está en el carrito, lo guardo en esta variable
+    const productoEnCarrito = this.estaEnCarrito(producto);
     if (productoEnCarrito) {
-      // Sumar cantidad
+      // Si está en el carrito, le sumo la cantidad
       productoEnCarrito.cantidad++;
     } else {
-      // Agregar al carrito
+      // Si no está, lo agrego al carrito
       this.carrito.push({ ...producto, cantidad: 1 });
-      localStorage.setItem("carrito", JSON.stringify(this.carrito));
     }
+    // Actualizo el storage
+    localStorage.setItem("carrito", JSON.stringify(this.carrito));
+    // Actualizo el carrito en el HTML
     this.listar();
   }
 
+  // Método para quitar o restar productos del carrito
   quitar(id) {
+    // Recibimos como parámetro el ID del producto, con ese ID buscamos el índice
+    // del producto para poder usar el splice y borrarlo en caso de que haga falta
     const indice = this.carrito.findIndex((producto) => producto.id === id);
     // Si la cantidad del producto es mayor a 1, le resto
     if (this.carrito[indice].cantidad > 1) {
       this.carrito[indice].cantidad--;
     } else {
-      // Sino, lo borro del carrito
+      // Sino, signica que hay un solo producto, así que lo borro
       this.carrito.splice(indice, 1);
     }
+    // Actualizo el storage
     localStorage.setItem("carrito", JSON.stringify(this.carrito));
+    // Actualizo el carrito en el HTML
     this.listar();
   }
 
+  // Este método es el encargado de actualizar el HTML de nuestro carrito
   listar() {
+    // Reiniciamos las variables
     this.total = 0;
     this.totalProductos = 0;
     divCarrito.innerHTML = "";
+    // Recorremos todos los productos del carrito y lo agregamos al div #carrito
     for (const producto of this.carrito) {
+      // A cada div lo agregamos un botón de Quitar del carrito, y a ese botón le pasamos
+      // el atributo data-id, con el id del producto. Eso después nos va a ser muy útil
+      // para saber desde que producto estamos haciendo click
       divCarrito.innerHTML += `
-        <div class="producto">
+        <div class="productoCarrito">
             <h2>${producto.nombre}</h2>
             <p>$${producto.precio}</p>
             <p>Cantidad: ${producto.cantidad}</p>
             <a href="#" data-id="${producto.id}" class="btnQuitar">Quitar del carrito</a>
         </div>
     `;
+      // Actualizamos los totales
       this.total += producto.precio * producto.cantidad;
       this.totalProductos += producto.cantidad;
     }
-    // Botones de quitar
+    // Botones de quitar: como no sabemos cuántos productos hay en el carrito,
+    // buscamos TODOS los botones que hayamos renderizado recién, y los recorremos uno por uno
     const botonesQuitar = document.querySelectorAll(".btnQuitar");
     for (const boton of botonesQuitar) {
+      // Le agregamos un evento onclick a cada uno
       boton.onclick = (event) => {
         event.preventDefault();
+        // Llamamos al método quitar, pasándole el ID del producto que sacamos
+        // del atributo data-id del HTML
         this.quitar(Number(boton.dataset.id));
       };
     }
@@ -106,7 +141,7 @@ class Carrito {
   }
 }
 
-// Clase molde para los productos
+// Clase "molde" para los productos
 class Producto {
   constructor(id, nombre, precio, categoria, imagen = false) {
     this.id = id;
@@ -125,48 +160,66 @@ const divProductos = document.querySelector("#productos");
 const divCarrito = document.querySelector("#carrito");
 const spanCantidadProductos = document.querySelector("#cantidadProductos");
 const spanTotalCarrito = document.querySelector("#totalCarrito");
-const formBuscar = document.querySelector("#formBuscar");
 const inputBuscar = document.querySelector("#inputBuscar");
+const botonCarrito = document.querySelector("section h1");
 
 // Llamamos a la función
 cargarProductos(bd.traerRegistros());
 
-// Muestra los registros de la base de datos en nuestro HTML
+// Esta función regular recibe como parámetro un array de productos y se encarga
+// de renderizarlos en el HTML
 function cargarProductos(productos) {
   divProductos.innerHTML = "";
+  // Recorremos todos los productos y lo agregamos al div #productos
   for (const producto of productos) {
+    // A cada div lo agregamos un botón de Agregar al carrito, y a ese botón le pasamos
+    // el atributo data-id, con el id del producto. Eso después nos va a ser muy útil
+    // para saber desde que producto estamos haciendo click
     divProductos.innerHTML += `
         <div class="producto">
             <h2>${producto.nombre}</h2>
-            <p>$${producto.precio}</p>
-            <img src="img/${producto.imagen}" width="150" />
-            <p><a href="#" class="btnAgregar" data-id="${producto.id}">Agregar al carrito</a></p>
+            <p class="precio">$${producto.precio}</p>
+            <div class="imagen">
+              <img src="img/${producto.imagen}" />
+            </div>
+            <a href="#" class="btnAgregar" data-id="${producto.id}">Agregar al carrito</a>
         </div>
     `;
   }
-  // Botones de agregar al carrito
+  // Botones agregar al carrito: como no sabemos cuántos productos hay en nuestra base de datos,
+  // buscamos TODOS los botones que hayamos renderizado recién, y los recorremos uno por uno
   const botonesAgregar = document.querySelectorAll(".btnAgregar");
   for (const boton of botonesAgregar) {
+    // Le agregamos un evento click a cada uno
     boton.addEventListener("click", (event) => {
       event.preventDefault();
+      // Obtenemos el ID del producto del atributo data-id
       const id = Number(boton.dataset.id);
+      // Con ese ID, consultamos a nuestra base de datos por el producto
       const producto = bd.registroPorId(id);
+      // Agregamos el registro (producto) a nuestro carrito
       carrito.agregar(producto);
     });
   }
 }
 
-// Evento buscador
-formBuscar.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const palabra = inputBuscar.value;
-  cargarProductos(bd.registrosPorNombre(palabra.toLowerCase()));
-});
+// Buscador: al soltar una tecla se ejecuta el evento keyup
 inputBuscar.addEventListener("keyup", (event) => {
   event.preventDefault();
+  // Obtenemos el atributo value del input
   const palabra = inputBuscar.value;
-  cargarProductos(bd.registrosPorNombre(palabra.toLowerCase()));
+  // Pedimos a nuestra base de datos que nos traiga todos los registros
+  // que coincidan con la palabra que pusimos en nuestro input
+  const productos = bd.registrosPorNombre(palabra.toLowerCase());
+  // Lo mostramos en el HTML
+  cargarProductos(productos);
 });
 
-// Objeto carrito
+// Trigger para ocultar/mostrar el carrito
+botonCarrito.addEventListener("click", (event) => {
+  document.querySelector("section").classList.toggle("ocultar");
+});
+
+// Objeto carrito: lo instanciamos a lo último de nuestro archivo JavaScript
+// para asegurarnos que TODO esté declarado e inicializado antes de crear el carrito
 const carrito = new Carrito();
